4a711e2e7e1b0ecd2a431bbfd111f65fd1926aa0
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-15 14:30:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def password_generator(length):
     charactors = string.ascii_letters + string.digits + "!@#$%^&*"
     return "".join(random.choice(charactors) for x in range(length))
 
+
+def password_strength_checker(password):
+    score = 0
+    common_passwords = ["12345678", "password123", "vikram123", "infinity999", "abcdefghij"]
+    if password in common_passwords:
+        score += 1
+
+
 st.title("Password Stength Checker🔏")
 
 password_length = st.number_input("Enter the password length",min_value=8, max_value=25, value=12)
