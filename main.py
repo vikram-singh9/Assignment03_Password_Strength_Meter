3d29133f6f7b0d8502347c8da165c9b4d5740565
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-15 14:11:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def password_generator(length):
     return "".join(random.choice(charactors) for x in range(length))
 
 st.title("Password Stength Checker🔏")
+
+password_length = st.number_input("Enter the password length",min_value=8, max_value=25, value=12)
+
+if st.button("Generate Password"):
+    password = password_generator(password_length)
+    st.info(password)
