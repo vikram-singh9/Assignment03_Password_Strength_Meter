18c284db5f7ae81b8c7bbb579091c020babbbeaf
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-15 15:43:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 import random
+import re
 import streamlit as st
 
 def password_generator(length):
@@ -17,12 +18,41 @@ def password_strength_checker(password):
     if len(password) >= 8:
         score +=1
     else:
-        return feedback.append("Password should be atleast 8 charactors! try again...")
+        feedback.append("Password should be atleast 8 charactors! try again😪")
+    
+    if re.search(r"[A-Z]",password) and re.search(r"[a-z]",password):
+        score += 1
+    else:
+        feedback.append("Include Uppercase and Lowercase🙄")
+    
+    if re.search(r"\d",password):
+        score += 1
+    else:
+        feedback.append("Add numbers btw 0-9🤨")
+
+    if re.search(r"[!@#$%^&*]",password):
+        score += 1
+    else:
+        feedback.append("Use special charactors🥱")
+
+    if score == 4:
+        return "💪🏽 Strong Password","strong"
+    if score == 3:
+        return "🧐 Weak password","moderate"
+    if score == 2:
+        return "\n".join(feedback),"weak"
     
 
 
 
 st.title("Password Stength Checker🔏")
+
+check_password = st.text_input("Enter Password to check **Strength**" ,type="password")
+if st.button("Check Strength"):
+   result, strength=  password_strength_checker(check_password)
+   if result == "strong":
+       st.success(result)
+       st.balloons()
 
 password_length = st.number_input("Enter the password length",min_value=8, max_value=25, value=12)
 
