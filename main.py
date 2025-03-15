576508285668f78e9fd364c9fe3e3ce2e5beb1e4
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-15 14:03:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import string
 import random
+import streamlit as st
 
-def password_generator():
-    charactors = string.ascii_letters + string.digits
-    
-    print(charactors)
+def password_generator(length):
+    charactors = string.ascii_letters + string.digits + "!@#$%^&*"
+    return "".join(random.choice(charactors) for x in range(length))
 
-
-print(password_generator())
+st.title("Password Stength Checker🔏")
